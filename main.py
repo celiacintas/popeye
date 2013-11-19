5f8799766d649c29fb876128ba52a16d0e98c8d6
Celita Cintas <cintas.celia@gmail.com>
2013-11-19 18:33:39 -0300

    new file:   Dependencies/__init__.py 	new file:   Dependencies/qimage2ndarray.py 	new file:   UI/Icons/__init_.py 	new file:   UI/Icons/about.png 	new file:   UI/Icons/alert.png 	new file:   UI/Icons/bn.png 	new file:   UI/Icons/cancel.png 	new file:   UI/Icons/exit.png 	new file:   UI/Icons/icono.png 	new file:   UI/Icons/images.png 	new file:   UI/Icons/info.png 	new file:   UI/Icons/learn.png 	new file:   UI/Icons/next.png 	new file:   UI/Icons/others.png 	new file:   UI/Icons/preferences.png 	new file:   UI/Icons/preferences_2.png 	new file:   UI/Icons/prev.png 	new file:   UI/Icons/run.png 	new file:   UI/Icons/test.png 	new file:   UI/__init__.py 	new file:   UI/mainWindow.py 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import sip
+sip.setapi('QString', 2)
+
+import sys
+from PyQt4 import QtGui, QtCore
+from UI.mainWindow import Main_Window
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    splash_pix = QtGui.QPixmap('Icons/test.png')
+    splash_pix = splash_pix.scaledToWidth(300)
+    splash = QtGui.QSplashScreen(splash_pix, QtCore.Qt.WindowStaysOnTopHint)
+    splash.show()
+
+    window = Main_Window()
+    window.move(300, 100)
+    window.show()
+    splash.finish(window)
+    window.bienvenida()
+
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
