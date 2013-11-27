f028e85979b53700efdddf7da2c165faf606bdef
Celita Cintas <cintas.celia@gmail.com>
2013-11-27 18:23:37 -0300

    add save to TPS format

--- Utils/export.py
+++ Utils/export.py
@@ -4,10 +4,12 @@ import os
 import numpy as np
 
 class SaveFile():
-	def __init__(self, fileName, landmarks):
-		print fileName
+	def __init__(self, fileName, landmarks, filterLandmarks):
 		self.name, self.ext = os.path.splitext(fileName)
 		self.landmarks = landmarks
+		#self.filterLandmarks = [[l[i].tolist() for i in filterLandmarks] for l in landmarks]
+		self.filterLandmarks = [[l[i] for i in filterLandmarks] for l in landmarks]
+		self.nfilter = len(filterLandmarks)
 
 	def saveXLS(self):
 		pass
@@ -21,11 +23,19 @@ class SaveFile():
 	def saveMorphoJ(self):
 		pass
 
+	def saveTPS(self):
+		with open(self.name + self.ext, 'w') as out:
+			for l in range(len(self.filterLandmarks)):
+				out.write(str("LM=" + str(self.nfilter) + '\n'))
+				map(lambda x: np.savetxt(out, self.filterLandmarks[l][x].reshape((1,2)), fmt='%i %i'), [i  for i in range(self.nfilter)])
+				out.write(str("ID="+str(l)+'\n'))
+			out.close()
+
 	def save(self):
 		if self.ext == ".xls":
 			self.saveXLS()
-		elif self.ext == ".txt":
-			self.saveTXT()
+		elif self.ext == ".tps":
+			self.saveTPS()
 
 
 
