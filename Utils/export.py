79c4594e134faf1f20f634191bfc554997fa6c59
Celita Cintas <cintas.celia@gmail.com>
2013-12-10 06:52:17 -0300

    change to ojoin

--- Utils/export.py
+++ Utils/export.py
@@ -7,8 +7,8 @@ class SaveFile():
 	def __init__(self, fileName, landmarks, filterLandmarks):
 		self.name, self.ext = os.path.splitext(fileName)
 		self.landmarks = landmarks
-		#self.filterLandmarks = [[l[i].tolist() for i in filterLandmarks] for l in landmarks]
 		self.filterLandmarks = [[l[i] for i in filterLandmarks] for l in landmarks]
+		print self.filterLandmarks
 		self.nfilter = len(filterLandmarks)
 
 	def saveXLS(self):
@@ -21,14 +21,14 @@ class SaveFile():
 		pass
 
 	def saveMorphoJ(self):
-		pass
+		pass	
 
 	def saveTPS(self):
 		with open(self.name + self.ext, 'w') as out:
 			for l in range(len(self.filterLandmarks)):
-				out.write(str("LM=" + str(self.nfilter) + '\n'))
+				out.write(str(self.nfilter).join(["LM=", '\n'])) 
 				map(lambda x: np.savetxt(out, self.filterLandmarks[l][x].reshape((1,2)), fmt='%i %i'), [i  for i in range(self.nfilter)])
-				out.write(str("ID="+str(l)+'\n'))
+				out.write(str(l).join(["ID=", '\n']))
 			out.close()
 
 	def save(self):
@@ -36,6 +36,9 @@ class SaveFile():
 			self.saveXLS()
 		elif self.ext == ".tps":
 			self.saveTPS()
+		elif self.ext == ".txt":
+			self.saveTXT()
+
 
 
 
