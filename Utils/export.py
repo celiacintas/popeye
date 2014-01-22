48587e125670cee1153d616faef0032a95c7624e
Celita Cintas <cintas.celia@gmail.com>
2014-01-22 16:55:33 -0300

    added ids codes and savetxt

--- Utils/export.py
+++ Utils/export.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+
 import os
+from os.path import basename
 import numpy as np
 
 
 class SaveFile():
 
-    def __init__(self, fileName, landmarks, filterLandmarks):
+    def __init__(self, imagesNames, fileName, landmarks, filterLandmarks):
         """
         Detect extension of file and filter landmarks to save
         """
@@ -15,36 +17,38 @@ class SaveFile():
         self.filterLandmarks = [[l[i] for i in filterLandmarks]
                                 for l in landmarks]
         self.nfilter = len(filterLandmarks)
+        self.idCodes = map(lambda p: os.path.splitext(basename(p))[0], imagesNames)
 
-    def saveXLS(self):
+    def saveXLS(self, out):
         pass
 
-    def saveTXT(self):
+    def saveTXT(self, out):
+        # TODO add code name of the ind
+        map(lambda x: np.savetxt(out, x, fmt='%-7.2f'), self.filterLandmarks)
+
+    def saveCVS(self, out):
         pass
 
-    def saveCVS(self):
+    def saveMorphoJ(self, out):
         pass
 
-    def saveMorphoJ(self):
-        pass
-
-    def saveTPS(self):
+    def saveTPS(self, out):
         """
         Save the chosen landmarks in TPS format
         http://life.bio.sunysb.edu/morph/soft-tps.html
         """
-        with open(''.join([self.name, self.ext]), 'w') as out:
-            for l in range(len(self.filterLandmarks)):
-                out.write(str(self.nfilter).join(["LM=", '\n']))
-                map(lambda x: np.savetxt(out, self.filterLandmarks[l][x].reshape(
-                    (1, 2)), fmt='%i %i'), [i for i in range(self.nfilter)])
-                out.write(str(l).join(["ID=", '\n']))
-            out.close()
+        for l in range(len(self.filterLandmarks)):
+            out.write(str(self.nfilter).join(["LM=", '\n']))
+            map(lambda x: np.savetxt(out, self.filterLandmarks[l][x].reshape(
+                (1, 2)), fmt='%i %i'), [i for i in range(self.nfilter)])
+            out.write(str(l).join(["ID=", '\n']))
 
     def save(self):
-        if self.ext == ".xls":
-            self.saveXLS()
-        elif self.ext == ".tps":
-            self.saveTPS()
-        elif self.ext == ".txt":
-            self.saveTXT()
+        with open(''.join([self.name, self.ext]), 'w') as out:
+            if self.ext == ".xls":
+                self.saveXLS(out)
+            elif self.ext == ".tps":
+                self.saveTPS(out)
+            elif self.ext == ".txt":
+                self.saveTXT(out)
+            out.close()
