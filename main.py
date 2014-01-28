498a0e3dd6e3e6fc274def649c37b9cebe807852
Celita Cintas <cintas.celia@gmail.com>
2014-01-28 15:27:56 -0300

    modified:   ../main.py

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import sip
 sip.setapi('QString', 2)
 
 import sys
+import os
 import logging
 from PyQt4 import QtGui, QtCore
 from UI.mainWindow import Main_Window
 
+dir = os.path.dirname(__file__)
+filenameLogs = os.path.join(dir, 'Logs/popeye.log')
 
 def main():
 
-    logging.basicConfig(filename='Logs/popeye.log')  # , level=logging.INFO)
+    logging.basicConfig(filename=filenameLogs)  # , level=logging.INFO)
     logging.info('Popeye Started')
     app = QtGui.QApplication(sys.argv)
 
