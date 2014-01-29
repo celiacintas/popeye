ef0fadf3164b87fc1b9ab3f2e1b7d22a89a580fc
Celita Cintas <cintas.celia@gmail.com>
2014-01-29 15:18:51 -0300

    cleanning code

--- Examples/test1.py
+++ Examples/test1.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+"""In this """
 
 import os
 import matplotlib.pyplot as plt
 from ..Dependencies import pystasm
 import skimage.io as io
 from skimage.draw import circle
-from skimage import img_as_ubyte
-import numpy as np
+
 
 
 FILENAME = os.path.join(os.path.dirname(__file__), "../Tests/Data/test.jpg")
 
 
 def main():
-    myStasm = pystasm.STASM()
-    myLandmarks = myStasm.s_search_single(FILENAME)
+    mystasm = pystasm.STASM()
+    mylandmarks = mystasm.s_search_single(FILENAME)
     image = io.imread(FILENAME)
-    for i in range(len(myLandmarks)):
-        rr, cc = circle(myLandmarks[i][1], myLandmarks[i][0], 15)
-        image[rr, cc] = 1
-    pepe = io.imshow(image)
+    for i in range(len(mylandmarks)):
+        radio, center = circle(mylandmarks[i][1], mylandmarks[i][0], 15)
+        image[radio, center] = 1
+    io.imshow(image)
     plt.show()
 
 if __name__ == '__main__':
