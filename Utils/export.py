d7e80c1dddd7ad4b505ef98a6ab8336d35b7c5c6
Celita Cintas <cintas.celia@gmail.com>
2014-01-29 15:18:01 -0300

    cleanning code

--- Utils/export.py
+++ Utils/export.py
@@ -6,55 +6,70 @@ from os.path import basename
 import numpy as np
 
 
-class SaveFile():
+class SaveFile(object):
 
-    def __init__(self, imagesNames, fileName, landmarks, filterLandmarks):
+    """This class is in charge of save in diferent formats
+    the landmarks obtained from the image (id, x, y, number of landmarks)"""
+
+    def __init__(self, imagesnames, filename, landmarks, filterlandmarks):
         """
         Detect extension of file and filter landmarks to save
         """
-        self.name, self.ext = os.path.splitext(fileName)
+        self.name, self.ext = os.path.splitext(filename)
         self.landmarks = landmarks
-        self.filterLandmarks = [[l[i] for i in filterLandmarks]
+        self.filterlandmarks = [[l[i] for i in filterlandmarks]
                                 for l in landmarks]
-        self.nfilter = len(filterLandmarks)
-        self.idCodes = map(lambda p: os.path.splitext(basename(p))[0],
-                           imagesNames)
+        self.nfilter = len(filterlandmarks)
+        self.idcodes = [os.path.splitext(basename(p))[0] for p in imagesnames]
 
-    def saveXLS(self, out):
+    def savexls(self, out):
+        """Save landmarks in spreadsheet"""
         pass
 
-    def saveTXT(self, out):
+    def savetxt(self, out):
+        """Save landmarks in txt (x, y)"""
         # TODO add code name of the ind
-        map(lambda x: np.savetxt(out, x, fmt='%i %i'), self.filterLandmarks)
+        map(lambda x: np.savetxt(out, x, fmt='%i %i'), self.filterlandmarks)
 
-    def saveCVS(self, out):
-
+    def savecvs(self, out):
+        """Save landmarks in cvs (x, y)"""
         pass
 
-    def saveMorphoJ(self, out):
-        for id in range(len(self.idCodes)):
-            out.write("".join([self.idCodes[id], ' ']))
-            np.savetxt(out, np.array(self.filterLandmarks[id]).reshape(
-                1, len(self.filterLandmarks[id]) * 2),  fmt='%i')
+    def savemorphoj(self, out):
+        """Save landmarks in txt whit morphoJ format
+        id (x y x y x y)*numberLandmarks"""
+        for i in range(len(self.idcodes)):
+            out.write("".join([self.idcodes[i], ' ']))
+            np.savetxt(out, np.array(self.filterlandmarks[i]).reshape(
+                1, len(self.filterlandmarks[i]) * 2), fmt='%i')
             # Change the shape for MorphoJ All landmarks in one line per person
 
-    def saveTPS(self, out):
+    def savetps(self, out):
         """
         Save the chosen landmarks in TPS format
         http://life.bio.sunysb.edu/morph/soft-tps.html
+        LM=numberLandmarks
+        x y
+        x y
+        ...
+        ...
+        id=numberId
         """
-        for l in range(len(self.filterLandmarks)):
+        for landmark in range(len(self.filterlandmarks)):
             out.write(str(self.nfilter).join(["LM=", '\n']))
-            map(lambda x: np.savetxt(out, self.filterLandmarks[l][x].reshape(
-                (1, 2)), fmt='%i %i'), [i for i in range(self.nfilter)])
-            out.write(str(l).join(["ID=", '\n']))
+            map(lambda x: np.savetxt(out,
+                self.filterlandmarks[landmark][x].reshape((1, 2)),
+                fmt='%i %i'), [i for i in range(self.nfilter)])
+            out.write(str(landmark).join(["ID=", '\n']))
 
     def save(self):
+        """Dependending on the extension file name .. the save
+        that we want to call"""
         with open(''.join([self.name, self.ext]), 'w') as out:
             if self.ext == ".xls":
-                self.saveXLS(out)
+                self.savexls(out)
             elif self.ext == ".tps":
-                self.saveTPS(out)
+                self.savetps(out)
             elif self.ext == ".txt":
-                self.saveMorphoJ(out)
+                self.savemorphoj(out)
             out.close()
--- Utils/qimage2ndarray.py
+++ Utils/qimage2ndarray.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-from PyQt4 import QtGui
-import numpy as np
- 
-def toQImage(im):
-    if im is None:
+from PyQt4.QtGui import QImage
+
+def toqimage(image):
+    if image is None:
         return QImage()
-    elif len(im.shape) == 3:
-        if im.shape[2] == 3:
-            qim = QtGui.QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QtGui.QImage.Format_RGB888);
+    elif len(image.shape) == 3:
+        if image.shape[2] == 3:
+            qim = QImage(image.data, image.shape[1],
+            	  image.shape[0], image.strides[0], QImage.Format_RGB888)
             return qim
-        elif im.shape[2] == 4:
-            qim = QtGui.QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QtGui.QImage.Format_ARGB32);
+        elif image.shape[2] == 4:
+            qim = QImage(image.data, image.shape[1],
+            	  image.shape[0], image.strides[0], QImage.Format_ARGB32)
             return qim
