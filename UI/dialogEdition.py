050b0c3b3ca4fa50fe37ccb90366268deed570f1
Celita Cintas <cintas.celia@gmail.com>
2014-03-17 12:27:36 -0300

    edition window

---
+++ UI/dialogEdition.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import os
+from PyQt4 import QtGui
+from opciones_UI import Ui_Dialog
+
+
+class DialogOptions(QtGui.QDialog):
+
+    def __init__(self, numberOfLandmarks=77, parent=None):
+        QtGui.QDialog.__init__(self)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.initUI(numberOfLandmarks)
+
+    def initUI(self, numberOfLandmarks):
+        pixmap = QtGui.QPixmap(
+            os.path.join(os.path.dirname(__file__), "Images/landmarking.png"))
+        for i in range(numberOfLandmarks):
+            item = QtGui.QListWidgetItem("%i" % i)
+            self.ui.listWidget.addItem(item)
+
+        self.ui.label.setPixmap(pixmap)
+
+        self.setWindowTitle('Landmarking Selection')
+        self.exec_()
